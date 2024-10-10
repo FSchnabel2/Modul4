f1efae21131b0753840995a90a9eb87cb435ceeb
FSchnabel2 <frauschnabel623@gmail.com>
2024-10-10 14:09:01 +0200

    Verschönern der Datumsausgabe von GregorianCalendar-Datenfeld in CSVExport

--- src/CSVExport.java
+++ src/CSVExport.java
@@ -1,10 +1,14 @@
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CSVExport extends Export {
     @Override
     public void export(ArrayList<Artikel> al) {
+
         for(Artikel a : al) {
-            System.out.println(a.getNummer()+";"+a.getBezeichnung()+";"+a.getEinkaufspreis()+";"+a.getAblaufdatum()+";"+a.getKategorienummer());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            String formatiertesDatum = a.getAblaufdatum().toZonedDateTime().format(formatter);
+            System.out.println(a.getNummer()+";"+a.getBezeichnung()+";"+a.getEinkaufspreis()+";"+formatiertesDatum+";"+a.getKategorienummer());
         }
     }
 }
